39a20bd946d79f780ede98593c8e9efd9633d76a
Evgenii <mie@crtweb.ru>
2019-11-01 22:08:24 +0500

    Добавлен PWA-манифест

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,13 +29,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Крафтовая пивоварня Gatsby`,
+        short_name: `Антиконфа в Тюмени`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/crt-icon.svg`, // This path is relative to the root of the site.
       },
     },
     {
--- gatsby-node.js
+++ gatsby-node.js
@@ -15,9 +15,9 @@ const makeRequest = (graphql, request) =>
 
 // Реализация функции Gatsby API "createPages". Она вызывается один раз когда
 // уровень данных готовится к работе для того, чтобы позволить плагину создать из этих данных страницы
-exports.createPages = ({ boundActionCreators, graphql }) => {
+exports.createPages = async ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators
-  const getBeers = makeRequest(
+  const getBeersAnswer = await makeRequest(
     graphql,
     `
       {
@@ -30,9 +30,9 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
         }
       }
       `
-  ).then(result => {
-    // Создаём страницы для каждого пива
-    result.data.allStrapiBeers.edges.forEach(({ node }) => {
+  )
+  const getBeers = getBeersAnswer.data.allStrapiBeers.edges.forEach(
+    ({ node }) => {
       createPage({
         path: `/${node.id}`,
         component: path.resolve(`src/pages/beer-page/index.js`),
@@ -40,8 +40,8 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
           id: node.id,
         },
       })
-    })
-  })
+    }
+  )
   // Запрашиваем материалы статей для использования при создании страниц.
   return getBeers
 }
